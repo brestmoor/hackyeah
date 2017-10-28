eb1fe12d8b5db34aaa3652719971b58db26ac868
Dawid Hujdus <dawid.hujdus@ericsson.com>
2017-10-29 00:28:05 +0200

    Change in query

--- src/main/java/hackyeah/weather/WeatherFetcher.java
+++ src/main/java/hackyeah/weather/WeatherFetcher.java
@@ -11,17 +11,24 @@ public class WeatherFetcher {
 
     public Set<String> fetchFromPoints(List<Point> pointList) {
         Set<String> weather = new HashSet<>();
-        for (Point point : pointList) {
-            weather.add(getWeatherForPoint(point).replace("\n", ""));
-        }
+        weather.add(getWeatherForPoint(pointList).replace("\n", ""));
         return weather;
     }
 
-    private String getWeatherForPoint(Point point) {
-        String query = String.format("select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text=\"(%s,%s)\")",
-                                     point.getLat(), point.getLng());
+    private String getWeatherForPoint(List<Point> point) {
+        String coords = prepareCoords(point);
+        String query = "select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE " + coords
+                + "\")";
         System.out.println(query);
         String stringResponse = UrlUtils.getWithQuery("https", "query.yahooapis.com", "v1/public/", query);
         return stringResponse;
+    }
+
+    private String prepareCoords(List<Point> point) {
+        String text = "";
+        for (Point p : point) {
+            text += "text=\"(" + p.getLat() + "," + p.getLng() + ")\" or ";
+        }
+        return text.substring(0, text.length() - 6);
     }
 }
