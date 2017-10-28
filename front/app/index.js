1ab56f5723dddd631e7534ec9d9bc0a8f56d913f
Kamila <kamila.sroda@vp.pl>
2017-10-28 15:58:33 +0200

    leaflet boilerplate

--- front/app/index.js
+++ front/app/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Map from './components/Map/Map'
+
 function App () {
   return (
     <div>
-      lalal
+      <Map />
     </div>
   )
 }
--- front/server.js
+++ front/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/assets/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/${req.originalUrl}`)
+})
+
 app.get('/bundle.js', (req, res) => {
   res.sendFile(`${__dirname}/dist/bundle.js`)
 })
