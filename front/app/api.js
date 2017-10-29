01893201cadee1eb119aa04100446d6167b7fbf8
Marek Łabuz <mrk.labuz@gmail.com>
2017-10-29 09:42:22 +0100

    https

--- front/app/api.js
+++ front/app/api.js
@@ -1,23 +1,24 @@
 import axios from 'axios'
 
-const baseURL = '5cg62261s4'
-const port = '8080'
+const protocol = 'https'
+const baseURL = 'coolweather.eu-gb.mybluemix.net'
+const port = '443'
 
 const api = {
-  search: text => axios.get(`http://${baseURL}:${port}/hackyeah/rest/cities?name=${text}`)
+  search: text => axios.get(`${protocol}://${baseURL}:${port}/rest/cities?name=${text}`)
     .then(({ data }) => data)
     .catch(() => []),
-  getLatLon: id => axios.get(`http://${baseURL}:${port}/hackyeah/rest/cities/id?id=${id}`)
+  getLatLon: id => axios.get(`${protocol}://${baseURL}:${port}/rest/cities/id?id=${id}`)
     .then(({ data }) => data)
     .catch(() => null),
-  mapBounds: (w, s, e, n) => axios.get(`http://${baseURL}:${port}/hackyeah/rest/cities/weather?n=${n}&e=${e}&s=${s}&w=${w}`)
+  mapBounds: (w, s, e, n) => axios.get(`${protocol}://${baseURL}:${port}/rest/cities/weather?n=${n}&e=${e}&s=${s}&w=${w}`)
     .then(({ data }) => ({
       data: data[0].query.results.channel,
       alerts: JSON.parse(data[0].alerts)
     }))
     .then(({ data, alerts }) => ({ data: (Array.isArray(data) ? data : [data]), alerts }))
     .catch(() => {}),
-  sendAnnotation: form => axios.post('http://5cg62261s4:8080/hackyeah/rest/alert', form)
+  sendAnnotation: form => axios.post(`${protocol}://${baseURL}:${port}/rest/alert`, form)
     .then(() => true)
     .catch(() => false)
 }
