aabd0cecb42f71fd4cfc98c8875360ef9ea1a428
Dawid Hujdus <dawid.hujdus@ericsson.com>
2017-10-29 02:04:23 +0200

    Bugfixes

--- src/main/java/hackyeah/weather/CityCSVReader.java
+++ src/main/java/hackyeah/weather/CityCSVReader.java
@@ -1,8 +1,9 @@
 package hackyeah.weather;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,26 +11,28 @@ import hackyeah.weather.dto.City;
 
 public class CityCSVReader {
 
-	public List<City> readAll() {
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader("Polska.csv"));
-			String line = bufferedReader.readLine();
-			int i=0;
-			List<City> cities = new ArrayList<>();
-			while (line != null) {
-				City city = makeCity(line, i);
-				cities.add(city);
-				i++;
-				line = bufferedReader.readLine();
-			}
-			return cities;
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public List<City> readAll() {
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("Polska.csv");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+            String line = bufferedReader.readLine();
+            int i = 0;
+            List<City> cities = new ArrayList<>();
+            while (line != null) {
+                City city = makeCity(line, i);
+                cities.add(city);
+                i++;
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+            return cities;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private City makeCity(String cityText, int i) {
-		String[] values = cityText.split(";");
-		return new City(values[0], "PL", String.valueOf(i), values[1], values[2]);
-	}
+    private City makeCity(String cityText, int i) {
+        String[] values = cityText.split(";");
+        return new City(values[0], "PL", String.valueOf(i), values[1], values[2]);
+    }
 }
